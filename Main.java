98dac8ebd044cdb3da309d62213bd2ea7f00b032
thenu <thenudeepi77@gmail.com>
2026-06-21 20:22:17 +0530

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+        System.out.println("Student Grade Management System");
+    }
+
+}
